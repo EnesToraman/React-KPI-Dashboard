ddc8f485cbabbe335ef3980f2d185907570be8df
EnesToraman <enstrmn@hotmail.com>
2021-12-22 18:00:08 +0300

    Create AuthContext.

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const signUp = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const logIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    const resetPassword = (email) => {
+        return sendPasswordResetEmail(auth, email);
+    }
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+
+        return unsub
+    }, [])
+
+    const value = {
+        currentUser,
+        signUp,
+        logIn,
+        logOut,
+        resetPassword,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
